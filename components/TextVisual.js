a5130fe583ddb52e5fc491fab3c98a1d82a006b2
Ronan <roleary81@gmail.com>
2022-06-30 00:45:06 +0200

    add TV component

--- components/TextVisual.js
+++ components/TextVisual.js
@@ -4,7 +4,7 @@ import styles from '../styles/TextVisual.module.css';
 const TextVisual = ({ content }) => {
    
     const { title, articleBody, image, buttons} = content;
-    // console.log(buttons);
+   
     return(
         <section className="b-textImage b-textImage--tint b-textVisual js-equinoxNode t-dark is-visible">
             <div className="b-textImage__imageContainer">
@@ -16,17 +16,17 @@ const TextVisual = ({ content }) => {
             <div className="o-wrapper">
                 <div className="b-textImage__text">
                     <h2 className="b-textImage__heading">{title}</h2>
-                    <p  dangerouslySetInnerHTML={{__html: articleBody}} />
+                    <p  dangerouslySetInnerHTML={{__html: articleBody }} />
+                    <div className="b-blocks__ctas">
                     {buttons && buttons.length > 0 ?
                         Object.entries(buttons).map((btn, i) => {
                             const { linkText, linkUrl } = btn[1];
                             return(
-                                <div key={i} className="b-blocks__ctas">
-                                    <Link href={`${linkUrl}`}><a className="c-button c-button--inverted">{linkText}</a></Link>
-                                </div> 
+                                <Link key={i} href={`${linkUrl}`}><a className="c-button c-button--inverted">{linkText}</a></Link>
                             )
                         })
                     : ''}
+                     </div> 
                 </div>
             </div>
         </section>
