25dbe3535876b78c2358f92a0dd28eacf591e3a9
Ronan <roleary81@gmail.com>
2022-07-10 23:48:06 +0200

    try try again

--- components/Faq.js
+++ components/Faq.js
@@ -12,9 +12,7 @@ const Faq = ({ content }) => {
     let selectorQuestion = '.js-faqQuestion';
 
     const onQuestionClick = useCallback((e) => {
-       
         const question = e.currentTarget;
-        console.log(question.parentNode);
         question.parentNode.classList.toggle(classNameIsExpanded);
     }, [classNameIsExpanded]);
 
@@ -27,7 +25,7 @@ const Faq = ({ content }) => {
         })
 
         questions.forEach(question => question.addEventListener('click', (e) => onQuestionClick(e)))
-    });
+    },[]);
 
     return(
         <section className="b-faq c-section js-faq">
