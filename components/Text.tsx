c431289fb82bd5fd9037cca548f3299ec722670c
ronan <ronan@thenextweb.com>
2022-08-16 18:54:03 +0200

    sopme more typescripting

--- components/Text.tsx
+++ components/Text.tsx
@@ -1,12 +1,11 @@
-interface IText {
+interface IProps {
     heading?: string
-    content?: any
+    column1?: string
+    column2?: string
 }
 
-const Text = ({ heading, content }: IText ): JSX.Element => {
-
-    let { column1 } = content; 
-
+const Text = ({ heading, column1, column2 }: IProps) => {
+    
     return(
         <section className="b-text  c-section" id="learn-more">
             <div className="o-wrapper">
@@ -15,7 +14,7 @@ const Text = ({ heading, content }: IText ): JSX.Element => {
                         <h2 className="b-text__heading">{heading}</h2>
                     </div>
                     <div className="o-grid__col l:o-grid__col--span-8">
-                        <div className={"c-formatted"} dangerouslySetInnerHTML={{__html: column1 }} />        
+                        {/* <div className={"c-formatted"} dangerouslySetInnerHTML={{__html: column1 }} />         */}
                     </div>
                 </div>
             </div>
--- components/TextVisual.tsx
+++ components/TextVisual.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode } from 'react';
+import React from 'react';
 import Link from 'next/link'
 import Image from 'next/image';
 import imageLoader from './../imageLoader'
